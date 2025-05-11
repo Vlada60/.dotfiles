dd739fd5ca25966929f0f85d7504ccf4ff7459ea
Vladimir Đorđević <vlada.dj@protonmail.com>
2025-05-11 02:51:36 +0200

    evdevtest

---
+++ keyboard-layout/test2.sh
@@ -0,0 +1,24 @@
+sed --debug -i '/<layout>/,/<\/layout>/ {
+/<name>custom<\/name>/ {
+:a
+$!N
+/<variantList\/>/!ba
+c\
+            <name>custom</name>\
+            <shortDescription>custom</shortDescription>\
+            <description>A Programmer layout</description>\
+            <languageList>\
+                <iso639Id>und</iso639Id>\
+            </languageList>\
+        </configItem>\
+        <variantList>\
+            <variant>\
+            <configItem>\
+                <name>latin</name>\
+                <description>Standard Layout</description>\
+            </configItem>\
+            </variant>\
+        </variantList>
+}
+}' evdevtest.xml
+
